ec0707c677bd7deabb6d9a37fd8fc7dad92afcfd
andré felipe <andre@andrefelipe.com>
2016-03-15 14:15:08 -0300

    added protection on functions include

---
+++ src/functions_include.php
@@ -0,0 +1,6 @@
+<?php
+
+// Don't redefine the functions if included multiple times.
+if (!function_exists('andrefelipe\Orchestrate\default_http_config')) {
+    require __DIR__ . '/functions.php';
+}
